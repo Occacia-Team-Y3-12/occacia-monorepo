002abfc3a1c19394c2dc5de0b1cb457c5cd43201
KASHMIKA DE SILVA <kashmikadesilva@gmail.com>
2025-12-24 00:41:55 +0530

    chore: add deployment pipeline final :(

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,16 @@
-print("Hello from Occacia")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Occacia API"}
+
+@app.get("/api/status")
+def get_status():
+    return {"status": "Running on Oracle Tank"}
+
+if __name__ == "__main__":
+    # You MUST bind to 0.0.0.0 for Docker to see it
+    uvicorn.run(app, host="0.0.0.0", port=8000)
