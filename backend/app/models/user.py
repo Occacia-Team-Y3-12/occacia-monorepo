5a7fb9fb9e484bd7f33a17bee0e0008e065db32a
Dilruk Jayasinghe <dilrukjayasinghe25@outlook.com>
2026-02-03 14:42:02 +0530

    OCA-49 Introduced new base entity and updatable entity for inheritance

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,25 +4,20 @@ from datetime import datetime, timezone
 from typing import Optional
 
 from app.common.enums import UserRole, UserStatus
+from app.models.base.updatable import UpdatableEntity
+
 
 def now_utc() -> datetime:
     return datetime.now(timezone.utc)
 
 @dataclass
-class User(ABC):
+class User(UpdatableEntity, ABC):
     user_id: str
     email: str
     password_hash: str
     role: UserRole
     status: UserStatus
     last_login_at: Optional[datetime] = None
-
-    # Use default_factory to create a new default value at runtime
-    created_at: datetime = field(default_factory=now_utc)
-    updated_at: datetime = field(default_factory=now_utc)
-
-    def mark_modified_time(self) -> None:
-        self.updated_at = now_utc()
 
     def mark_login_time(self) -> None:
         self.last_login_at = now_utc()
