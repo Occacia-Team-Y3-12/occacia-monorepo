237587ac0f8cac82289014ae8af00eb6ae57dc93
Dilruk Jayasinghe <dilrukjayasinghe25@outlook.com>
2026-02-03 14:36:34 +0530

    OCA-49 Introduced user, customer entities with enums
    # Conflicts:
    #	backend/app/models/customer.py
    #	backend/app/models/user.py

---
+++ backend/app/common/enums/__init__.py
@@ -0,0 +1,19 @@
+from .enums import (
+    UserRole,
+    UserStatus,
+    OrderStatus,
+    OrderActivityStatus,
+    VendorApprovalStatus,
+    PlanType,
+    InquiryStatus,
+)
+
+__all__ = [
+    "UserRole",
+    "UserStatus",
+    "OrderStatus",
+    "OrderActivityStatus",
+    "VendorApprovalStatus",
+    "PlanType",
+    "InquiryStatus",
+]
