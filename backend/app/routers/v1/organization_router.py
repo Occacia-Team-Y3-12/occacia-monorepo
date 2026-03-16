cbd24b015cd0033ee8d682b57c3426bb3f854430
Dilruk Jayasinghe <dilrukjayasinghe25@outlook.com>
2026-03-16 05:41:13 +0530

    OCA-308 Removed legacy codes from backend and fixed deployment

--- backend/app/routers/v1/organization_router.py
+++ backend/app/routers/v1/organization_router.py
@@ -2,7 +2,7 @@
 
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
